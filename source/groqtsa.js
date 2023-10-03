82efa7343cd14f763f273680f3301666b1e29181
Ian Prins <iprins@groq.com>
2023-10-03 16:20:28 -0700

    Remove unused attribute class

--- source/groqtsa.js
+++ source/groqtsa.js
@@ -125,22 +125,6 @@ groqtsa.Argument = class {
     }
 };
 
-groqtsa.Attribute = class {
-
-    constructor(metadata, name, value) {
-        this._name = name;
-        this._value = value;
-    }
-
-    get name() {
-        return this._name;
-    }
-
-    get value() {
-        return this._value;
-    }
-};
-
 groqtsa.Error = class extends Error {
 
     constructor(message) {
